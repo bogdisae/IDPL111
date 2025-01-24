02fd774608a13cfe94bc3bd7c9a9a402b6ec6337
JamesEverett28 <je477@cam.ac.uk>
2025-01-24 16:31:47 +0000

    basic line follower

--- BOT.py
+++ BOT.py
@@ -1,20 +1,37 @@
+from machine import Pin, PWM
+from MOTOR import Motor
+
 class Bot:
-    # Line sensors: 0 = black, 1 = white
-    s_lineL = True #left
-    s_lineR = False #right
-    running = True
+    
+    def __init__(self):
+    
+        # Assign motors
+        self.L_motor = Motor(1)
+        self.R_motor = Motor(2)
+    
+        # Line sensors: 0 = black, 1 = white
+        self.s_lineL = True #left
+        self.s_lineR = False #right
+        self.running = True
+        
+    def bank_L(self):
+        self.L_motor.Forward(80)
+        self.R_motor.Forward(100)
+        
+    def bank_R(self):
+        self.L_motor.Forward(100)
+        self.R_motor.Forward(80)
+        
+    def forward(self):
+        self.L_motor.Forward(80)
+        self.R_motor.Forward(80)
     
     def line_following_onoff(self):
-        if(self.s_lineL and (not self.s_lineR)):
-            self.right()
+        if (self.s_lineL and (not self.s_lineR)):
+            self.bank_R()
         elif ((not self.s_lineL) and self.s_lineR):
-            self.left()
-    
-    def left(self):
-        print("turning left")
-    
-    def right(self):
-        print("turning right")
+            self.bank_L()
+        else self.forward()
     
     def run(self):
         while(self.running):
--- MOTOR.py
+++ MOTOR.py
@@ -3,22 +3,31 @@ from utime import sleep
 
 class Motor:
     
-    def __init__(self):
-        self.m1Dir = Pin(7 , Pin.OUT) # set motor direction
-        self.pwm1 = PWM(Pin(6)) # set speed
-        self.pwm1.freq(1000) # set max frequency
-        self.pwm1.duty_u16(0) # set duty cycle
+    def __init__(self, motor_no):
+        
+        if motor_no == 1:
+            Dir_pin = 7
+            PWM_pin = 6
+            
+        elif motor_no == 2:
+            Dir_pin = 3
+            PWM_pin = 2
+            
+        self.Dir = Pin(Dir_pin , Pin.OUT) # set motor direction
+        self.pwm = PWM(Pin(PWM_pin)) # set speed
+        self.pwm.freq(1000) # set max frequency
+        self.pwm.duty_u16(0) # set duty cycle
  
     def off(self):
-        self.pwm1.duty_u16(0)
+        self.pwm.duty_u16(0)
         
     def Forward(self, speed): # speed range 0-100
-        self.m1Dir.value(0)
-        self.pwm1.duty_u16(int(65535*speed/100))
+        self.Dir.value(0)
+        self.pwm.duty_u16(int(65535*speed/100))
         
     def Reverse(self, speed): # speed range 0-100
-        self.m1Dir.value(1)
-        self.pwm1.duty_u16(int(65535*speed/100))
+        self.Dir.value(1)
+        self.pwm.duty_u16(int(65535*speed/100))
         
         
 
