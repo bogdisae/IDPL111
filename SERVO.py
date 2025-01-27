5fa68deaa2f0da427cc320ea4e66e290c005f566
Bogdan Isaev <57876472+bogdisae@users.noreply.github.com>
2025-01-27 15:28:52 +0000

    Update SERVO.py

--- SERVO.py
+++ SERVO.py
@@ -1,8 +1,6 @@
 from machine import Pin, PWM
 from time import sleep
-# Set up PWM Pin for servo control
-servo_pin = machine.Pin(15)
-servo = PWM(servo_pin)
+
 # Set Duty Cycle for Different Angles
 max_duty = 7864
 min_duty = 1802
@@ -12,25 +10,19 @@ class Servo:
     
     def __init__(self):
         self.Dir = Pin(Dir_pin , Pin.OUT) # set motor direction
-        self.pwm = PWM(Pin(PWM_pin)) # set speed
+        self.pwm = PWM(Pin(15)) # set speed
         self.pwm.freq(50) # set max frequency
-        # Set Duty Cycle for Different Angles
-        self.pwm.max_duty = 7864
-        self.pwm.min_duty = 1802
-        self.pwm.half_duty = int(max_duty/2)
+        self.pwm.duty_u16(1802)
+        
+    def off(self): #Servo at 0 degrees
+        self.pwm.duty_u16(1802)
+        
+    def half (self): #Servo at 90 degrees
+        self.pwm.duty_u16 (int(7864))
+        
+    def full (self, speed): #Servo at 180 degrees
+        self.pwm.duty_u16 (7864)
+        
+    
+             
 
-try:
-    while True:
-        #Servo at 0 degrees
-        servo.duty_u16(min_duty)
-        sleep(2)
-        #Servo at 90 degrees
-        servo.duty_u16(half_duty)
-        sleep(2)
-        #Servo at 180 degrees
-        servo.duty_u16(max_duty)
-        sleep(2)
-except KeyboardInterrupt:
- print("Keyboard interrupt")
- # Turn off PWM
- servo.deinit()
