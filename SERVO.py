f5d9b5cc0d7f3363864b23882c1f36830aff1ad4
Bogdan Isaev <57876472+bogdisae@users.noreply.github.com>
2025-01-27 12:02:12 +0000

    Update SERVO.py

--- SERVO.py
+++ SERVO.py
@@ -1,20 +1,36 @@
 from machine import Pin, PWM
-from utime import sleep
+from time import sleep
+# Set up PWM Pin for servo control
+servo_pin = machine.Pin(15)
+servo = PWM(servo_pin)
+# Set Duty Cycle for Different Angles
+max_duty = 7864
+min_duty = 1802
+half_duty = int(max_duty/2)
 
 class Servo:
+    
     def __init__(self):
-        self.Dir = Pin(8 , Pin.OUT) # set motor direction
-        self.pwm = PWM(9) # set speed
-        self.pwm.freq(1000) # set max frequency
-        self.pwm.duty_u16(0) # set duty cycle
- 
-    def off(self):
-        self.pwm.duty_u16(0)
-        
-    def Forward(self, speed): # speed range 0-100
-        self.Dir.value(0)
-        self.pwm.duty_u16(int(65535*speed/100))
-        
-    def Reverse(self, speed): # speed range 0-100
-        self.Dir.value(1)
-        self.pwm.duty_u16(int(65535*speed/100))
+        self.Dir = Pin(Dir_pin , Pin.OUT) # set motor direction
+        self.pwm = PWM(Pin(PWM_pin)) # set speed
+        self.pwm.freq(50) # set max frequency
+        # Set Duty Cycle for Different Angles
+        self.pwm.max_duty = 7864
+        self.pwm.min_duty = 1802
+        self.pwm.half_duty = int(max_duty/2)
+
+try:
+    while True:
+        #Servo at 0 degrees
+        servo.duty_u16(min_duty)
+        sleep(2)
+        #Servo at 90 degrees
+        servo.duty_u16(half_duty)
+        sleep(2)
+        #Servo at 180 degrees
+        servo.duty_u16(max_duty)
+        sleep(2)
+except KeyboardInterrupt:
+ print("Keyboard interrupt")
+ # Turn off PWM
+ servo.deinit()
