a1df59babbabc59132a62ca7d844d1e80a12fa22
JamesEverett28 <je477@cam.ac.uk>
2025-01-29 09:15:05 +0000

    turning

--- BOT.py
+++ BOT.py
@@ -1,16 +1,25 @@
-from machine import Pin
+from machine import Pin, PWM
 from MOTOR import Motor
 import time
 
 class Bot:
     def __init__(self):
+        
+        # MOTORS:
         self.R_motor = Motor(4, 5)  # Right Motor
         self.L_motor = Motor(7, 6)  # Left Motor
+        
+        # SENSORS
         self.sensor_left = Pin(10, Pin.IN)  # Left (off the line) sensor
         self.sensor_middle_left = Pin(11, Pin.IN) # Middle left (on the line) sensor
-        self.sensor_middle_right = Pin(11, Pin.IN) # Middle right (on the line) sensor
-        self.sensor_right = Pin(12, Pin.IN)  # Right (off the line) sensor
-        self.running = True  
+        self.sensor_middle_right = Pin(12, Pin.IN) # Middle right (on the line) sensor
+        self.sensor_right = Pin(13, Pin.IN)  # Right (off the line) sensor
+        
+        # TURNING
+        self.turning = False
+        self.turn_time = 0 
+        
+        self.running = True
 
     def update_sensors(self):
         self.s_lineL = self.sensor_left.value()  # 0=BLACK 1=WHITE
@@ -34,25 +43,61 @@ class Bot:
         self.L_motor.Forward(80)
         self.R_motor.Forward(80)
         time.sleep(0.5)
+            
+    def turn(self):
+        direction = "L" # this has been included for testing. In the future this will be retrieved from a navigation module
         
-    def initiate_turning(self):            
-
+        # allow time for centre of turning of bot to reach junction (need to experimentally tune this time)
+        if time.time() - self.turn_time < 1:
+            self.follow_line()
+        
+        # turn the bot
+        else: 
+            if direction == "L":
+                self.L_motor.Reverse(50)
+                self.R_motor.Forward(50)
+            
+            elif direction == "R":
+                self.L_motor.Forward(50)
+                self.R_motor.Reverse(50)
+        
+        # stop turning when middle sensor reads the line again (needs adjusting to make sure the correct sensor is used)
+        if (time.time() - self.turn_time > 1.5) and (self.s_lineML == 1):
+            self.turning = False
+            
+            
     def follow_line(self):
+        
+        if self.s_lineML == 1 and self.s_lineMR == 1: #on the line 
+            self.forward()
+        elif self.s_lineML == 1 and self.s_lineMR == 0: #too right 
+            self.bank_L()
+        elif self.s_lineML == 0 and self.s_lineMR == 1: #too left
+            self.bank_R()
+        else: #off the line
+            pass
+        
+    def drive(self):
+        
         self.update_sensors()
-        if self.s_lineL == 1 or self.s_lineR == 1: #
-            initiate_turning()
-        else:            
-            if self.s_lineML == 1 and self.s_lineMR == 1: #on the line 
-                self.forward()
-            elif self.s_lineML == 1 and self.s_lineMR == 0: #too right 
-                self.bank_L()
-            elif self.s_lineML == 0 and self.s_lineMR == 1: #too left
-                self.bank_R()
-            else: #off the line
-                pass
+        
+        if not self.turning:
+            self.follow_line()
+        
+        # if the far left/right sensors detect a junction, trigger turning sequence
+        if (self.s_lineL == 1 or self.s_lineR == 1) and not self.turning:
+                self.turn_time = time.time()
+                self.turning = True
+                
+        if self.turning:
+                self.turn()
         
     def run(self):
         while self.running:
-            self.follow_line()
+            self.drive()
+                
+
+
+
 
 
