0de572fde8e4960089d92c4c0d3087147667ccfd
Alyssa831 <147514518+Alyssa831@users.noreply.github.com>
2025-01-30 09:22:02 +0000

    Update camera_test.py

--- camera_test.py
+++ camera_test.py
@@ -2,7 +2,7 @@ import struct
 import time
 import machine
 import distance  
-from SERVO import SERVO     
+from SERVO import Servo     
 
 # QR Code Reader Setup
 TINY_CODE_READER_I2C_ADDRESS = 0x0C
@@ -18,13 +18,15 @@ TINY_CODE_READER_I2C_BYTE_COUNT = struct.calcsize(TINY_CODE_READER_I2C_FORMAT)
 # Initialize I2C for QR Code Reader
 i2c = machine.I2C(1, scl=machine.Pin(19), sda=machine.Pin(18), freq=400000)
 
+Servo=Servo()
+
 def detect_qr_code():
     while True:
         time.sleep(TINY_CODE_READER_DELAY)
         read_data = i2c.readfrom(TINY_CODE_READER_I2C_ADDRESS, TINY_CODE_READER_I2C_BYTE_COUNT)
         message_length, = struct.unpack_from(TINY_CODE_READER_LENGTH_FORMAT, read_data, TINY_CODE_READER_LENGTH_OFFSET)
         message_bytes = struct.unpack_from(TINY_CODE_READER_MESSAGE_FORMAT, read_data, TINY_CODE_READER_MESSAGE_OFFSET)
-
+        
         if message_length == 0:
             print('No QR code detected')
             continue
@@ -36,8 +38,10 @@ def detect_qr_code():
             distance_value = distance.get_distance()
             print("Measured Distance:", distance_value)
             if distance_value <= 5:
-                servo.activate_servo()
+                Servo.activate_servo()
 
         except Exception as e:
             print("Error decoding QR code:", e)
             pass
+
+
