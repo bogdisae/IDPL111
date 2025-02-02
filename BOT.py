bc6e270dfec7cc42bd9326490ce6642cf6be5716
JamesEverett28 <je477@cam.ac.uk>
2025-02-02 00:44:47 +0000

    Moved paths variable to another module

--- BOT.py
+++ BOT.py
@@ -1,87 +1,11 @@
 from machine import Pin
+from PATHS import PATHS
 from MOTOR import Motor
 from CAMERA import Camera
 from DISTANCE import Distance
 from SERVO import Servo
 from LIGHT import Light
 import time
-
-PATHS = {
-    "LA": ["right", "left"],  # Left warehouse to A
-    "LB": [
-        "straight",
-        "right",
-        "straight",
-        "right",
-    ],  # Left warehouse to B
-    "LC": [
-        "straight",
-        "right",
-        "left",
-        "left",
-    ],  # Left warehouse to C
-    "LD": [
-        "straight",
-        "straight",
-        "right",
-        "straight",
-        "right",
-    ],  # Left warehouse to D
-    "RA": ["left", "straight", "right"],  # Right warehouse to A
-    "RB": ["straight", "left", "left"],  # Right warehouse to B
-    "RC": [
-        "straight",
-        "left",
-        "straight",
-        "right",
-        "left",
-    ],  # Right warehouse to C
-    "RD": [
-        "straight",
-        "straight",
-        "right",
-        "left",
-    ],  # Right warehouse to D
-    "AL": ["right", "left"],  # A to Left warehouse
-    "BL": [
-        "left",
-        "straight",
-        "left",
-        "straight",
-    ],  # B to Left warehouse
-    "CL": [
-        "right",
-        "right",
-        "left",
-        "straight",
-    ],  # C to Left warehouse
-    "DL": [
-        "left",
-        "straight",
-        "left",
-        "straight",
-        "straight",
-    ],  # D to Left warehouse
-    "AR": ["left", "straight", "right"],  # A to Right warehouse
-    "BR": ["right", "right", "straight"],  # B to Right warehouse
-    "CR": [
-        "right",
-        "left",
-        "straight",
-        "right",
-        "straight",
-    ],  # C to Right warehouse
-    "DR": [
-        "right",
-        "right",
-        "straight",
-        "straight",
-    ],  # D to Right warehouse
-    "HL": ["left", "straight", "left"],  # Home to Left warehouse
-    "HR": ["right", "right"],  # Home to Right warehouse
-    "LH": ["right", "straight", "right"],  # Left warehouse to Home
-    "RH": ["left", "left"],  # Right warehouse to Home
-}
    
 class Bot:
     def __init__(self):
---
+++ PATHS.py
@@ -0,0 +1,76 @@
+PATHS = {
+    "LA": ["right", "left"],  # Left warehouse to A
+    "LB": [
+        "straight",
+        "right",
+        "straight",
+        "right",
+    ],  # Left warehouse to B
+    "LC": [
+        "straight",
+        "right",
+        "left",
+        "left",
+    ],  # Left warehouse to C
+    "LD": [
+        "straight",
+        "straight",
+        "right",
+        "straight",
+        "right",
+    ],  # Left warehouse to D
+    "RA": ["left", "straight", "right"],  # Right warehouse to A
+    "RB": ["straight", "left", "left"],  # Right warehouse to B
+    "RC": [
+        "straight",
+        "left",
+        "straight",
+        "right",
+        "left",
+    ],  # Right warehouse to C
+    "RD": [
+        "straight",
+        "straight",
+        "right",
+        "left",
+    ],  # Right warehouse to D
+    "AL": ["right", "left"],  # A to Left warehouse
+    "BL": [
+        "left",
+        "straight",
+        "left",
+        "straight",
+    ],  # B to Left warehouse
+    "CL": [
+        "right",
+        "right",
+        "left",
+        "straight",
+    ],  # C to Left warehouse
+    "DL": [
+        "left",
+        "straight",
+        "left",
+        "straight",
+        "straight",
+    ],  # D to Left warehouse
+    "AR": ["left", "straight", "right"],  # A to Right warehouse
+    "BR": ["right", "right", "straight"],  # B to Right warehouse
+    "CR": [
+        "right",
+        "left",
+        "straight",
+        "right",
+        "straight",
+    ],  # C to Right warehouse
+    "DR": [
+        "right",
+        "right",
+        "straight",
+        "straight",
+    ],  # D to Right warehouse
+    "HL": ["left", "straight", "left"],  # Home to Left warehouse
+    "HR": ["right", "right"],  # Home to Right warehouse
+    "LH": ["right", "straight", "right"],  # Left warehouse to Home
+    "RH": ["left", "left"],  # Right warehouse to Home
+}
