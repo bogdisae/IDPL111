87152d09dc87858afcd804a87b7b1a1883990fc3
Yuming994 <156856069+Yuming994@users.noreply.github.com>
2025-02-03 15:43:06 +0000

    Update CAMERA.py

--- CAMERA.py
+++ CAMERA.py
@@ -23,6 +23,7 @@ class Camera:
         self.i2c = machine.I2C(1, scl=machine.Pin(19), sda=machine.Pin(18), freq=400000)
 
     def detect_qr_code(self):
+        self.detected_qr = False
 
         time.sleep(self.TINY_CODE_READER_DELAY)
         self.read_data = self.i2c.readfrom(self.TINY_CODE_READER_I2C_ADDRESS, self.TINY_CODE_READER_I2C_BYTE_COUNT)
@@ -33,10 +34,13 @@ class Camera:
             try:
                 self.message_string = bytearray(self.message_bytes[0:self.message_length]).decode("utf-8")
                 self.detected_qr = True
+                time.sleep(3)
 
             except Exception as e:
                 #print("Error decoding QR code:", e)
                 pass
+
+
 
 
 
